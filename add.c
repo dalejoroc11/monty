1a2a8dd9f823c213dcbb74ce383c6f719ee31a01
dalejoroc11 <diegorojas279@hotmail.com>
2020-05-15 00:55:51 +0000

    monty

--- add.c
+++ add.c
@@ -1,19 +1,29 @@
 #include "monty.h"
 /**
- * add - entry point
- * @stack: stack_t variable
- * @line_number: unsigned int variable
+ * add - add top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-unsigned int sum;
-if ((*stack == NULL) || ((*stack)->next == NULL && (*stack)->prev == NULL))
+stack_t *tmp = *stack;
+int sum = 0, i = 0;
+if (tmp == NULL)
 {
 fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 exit(EXIT_FAILURE);
 }
-sum = (*stack)->n + (*stack)->next->n;
-pop(stack, 0);
-pop(stack, 0);
-push(stack, sum);
+while (tmp)
+{
+tmp = tmp->next;
+i++;
+}
+if (stack == NULL || (*stack)->next == NULL || i <= 1)
+{
+fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
+sum = (*stack)->next->n + (*stack)->n;
+pop(stack, line_number);
+(*stack)->n = sum;
 }
